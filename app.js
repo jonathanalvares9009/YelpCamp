20f73a3c9982f185c98cd4957f7f153c81f5e5c8
Jonathan Alvares <alvaresjonathan599@gmail.com>
2020-12-06 18:45:12 +0530

    Added styles to YelpCamp on Dec 6 2020 at 6.45 p.m.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useFindAndModify: true, useCreateIndex: true, useUnifiedTopology: true });
 
@@ -18,6 +19,8 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+
+app.engine('ejs', ejsMate);
 
 app.get('/', (req, res) => {
     res.render('home.ejs');
--- models/campground.js
+++ models/campground.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
     title: String,
-    price: String,
-    desciption: String,
+    image: String,
+    price: Number,
+    description: String,
     location: String
-}
-);
+});
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
