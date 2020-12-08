afbdce867ebe304e101e751678b9a2ff7968de35
Jonathan Alvares <alvaresjonathan599@gmail.com>
2020-12-08 13:21:42 +0530

    Added new features

--- seeds/index.js
+++ seeds/index.js
@@ -1,34 +1,40 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
-const Campground = require('../models/campground');
 const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useFindAndModify: true, useCreateIndex: true });
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Connection Error'));
-db.once('open', () => {
-    console.log('Database connected');
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
 });
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
+
 
 const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() * 30) + 10;
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus enim eos saepe esse! Libero optio dolorum, est itaque maiores nobis nisi minus delectus, in unde magnam beatae esse qui voluptatem!'
-        });
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            price
+        })
         await camp.save();
     }
-};
+}
 
-seedDB().
-    then(() => {
-        mongoose.connection.close;
-    });
+seedDB().then(() => {
+    mongoose.connection.close();
+})
