2873b822afc29599429d20294db12e0cb1b10486
Jonathan Alvares <alvaresjonathan599@gmail.com>
2020-12-21 13:54:24 +0530

    It is finally completed after 2 months. 1.54pm 21/12/2020

---
+++ README.md
@@ -0,0 +1,23 @@
+**YelpCamp** is a website that lists all the campgounds stored in the database.
+
+To install my code on your computer, you need **Node.js**.
+
+The second step is to go into the terminal, then go into th project directory and then use **npm i**. This command installs all the dependencies.
+
+You can get view the webpage by running **node app.js**
+
+If you want to make some changes to the code fork it to your folder and then make changes and feel free to make pull request and send it.
+
+Here's the project structure:
+
+- **cloudinary folder** contains the code used to stored the photos used on the site.
+- **controllers folder** contains all the routing controllers and they are divided further into logical files.
+- **models folder** contains all the models relating to mongoDB schemas.
+- **public folder** contains stylesheets and javascript files aka static files.
+- **routes folder** contains all the routes and they are divided further into logical files.
+- **seeds folder** contains all the seeding files to fill the database.
+- **utils folder** contains all the general us js files.
+- **views folder** contains all the **HTNL** and **CSS** code.
+- **app.js** is the heart of the application it is run before anything.
+- **middleware.js** contains all the middleware.
+- **schemas.js** contains all the mongoDB error testing.
--- app.js
+++ app.js
@@ -2,7 +2,6 @@ if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
 
-
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -14,11 +13,8 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
-// const helmet = require('helmet');
 
 const mongoSanitize = require('express-mongo-sanitize');
-
-
 
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
@@ -77,54 +73,6 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
-// app.use(helmet());
-
-
-// const scriptSrcUrls = [
-//     "https://stackpath.bootstrapcdn.com/",
-//     "https://api.tiles.mapbox.com/",
-//     "https://api.mapbox.com/",
-//     "https://kit.fontawesome.com/",
-//     "https://cdnjs.cloudflare.com/",
-//     "https://cdn.jsdelivr.net",
-// ];
-// const styleSrcUrls = [
-//     "https://kit-free.fontawesome.com/",
-//     "https://stackpath.bootstrapcdn.com/",
-//     "https://api.mapbox.com/",
-//     "https://api.tiles.mapbox.com/",
-//     "https://fonts.googleapis.com/",
-//     "https://use.fontawesome.com/",
-// ];
-// const connectSrcUrls = [
-//     "https://api.mapbox.com/",
-//     "https://a.tiles.mapbox.com/",
-//     "https://b.tiles.mapbox.com/",
-//     "https://events.mapbox.com/",
-// ];
-// const fontSrcUrls = [];
-// app.use(
-//     helmet.contentSecurityPolicy({
-//         directives: {
-//             defaultSrc: [],
-//             connectSrc: ["'self'", ...connectSrcUrls],
-//             scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-//             styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-//             workerSrc: ["'self'", "blob:"],
-//             objectSrc: [],
-//             imgSrc: [
-//                 "'self'",
-//                 "blob:",
-//                 "data:",
-//                 "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
-//                 "https://images.unsplash.com/",
-//             ],
-//             fontSrc: ["'self'", ...fontSrcUrls],
-//         },
-//     })
-// );
-
-
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -139,16 +87,13 @@ app.use((req, res, next) => {
     next();
 })
 
-
 app.use('/', userRoutes);
 app.use('/campgrounds', campgroundRoutes)
 app.use('/campgrounds/:id/reviews', reviewRoutes)
 
-
 app.get('/', (req, res) => {
     res.render('home')
 });
-
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404))
